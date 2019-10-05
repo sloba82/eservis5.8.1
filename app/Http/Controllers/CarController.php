01511b3cb524b17bc593d87c5cd7c42610af194b
sloba82 <dizajnistampa@gmail.com>
2019-10-05 20:42:30 +0200

    empty push

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -64,7 +64,6 @@ class CarController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
