661dc4c2862e502bdb33ab47b9534f51e0c68986
sloba82 <dizajnistampa@gmail.com>
2020-02-04 20:06:57 +0100

    working on some issues with vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,35 +10,46 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import './custom.scss'
+
+
+
 
 // This imports all the layout components such as <b-container>, <b-row>, <b-col>:
 import { LayoutPlugin } from 'bootstrap-vue'
-Vue.use(LayoutPlugin)
+Vue.use(LayoutPlugin);
 
 // This imports <b-modal> as well as the v-b-modal directive as a plugin:
 import { ModalPlugin } from 'bootstrap-vue'
-Vue.use(ModalPlugin)
+Vue.use(ModalPlugin);
 
 // This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
 import { CardPlugin } from 'bootstrap-vue'
-Vue.use(CardPlugin)
+Vue.use(CardPlugin);
 
 // This imports directive v-b-scrollspy as a plugin:
 import { VBScrollspyPlugin } from 'bootstrap-vue'
-Vue.use(VBScrollspyPlugin)
+Vue.use(VBScrollspyPlugin);
 
 // This imports the dropdown and table plugins
 import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
-Vue.use(DropdownPlugin)
-Vue.use(TablePlugin)
+Vue.use(DropdownPlugin);
+Vue.use(TablePlugin);
+
+
+import Vuetify from 'vuetify'
+Vue.use(Vuetify);
+
+
+
+
+
 
 require('./bootstrap');
 
 
 window.Vue = require('vue');
 
-import Vuetify from 'vuetify'
-Vue.use(Vuetify)
 
 
 /**
@@ -62,4 +73,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
 });
